61ef3bf95a871718988236722ef1035817b6d421
Aisha Amir <aishaamirkhann@gmail.com>
2026-07-14 01:44:05 +0500

    Add files via upload

---
+++ Assigment4/random.py
@@ -0,0 +1,9 @@
+import random
+
+print("random number = ", random.randint(50,100))
+
+print(" OTP =", random.randint(100000,999999))
+
+fruits = ["Apple","Banana","Mango","Orange","Grapes"]
+
+print("Random Fruit =", random.choice(fruits))
