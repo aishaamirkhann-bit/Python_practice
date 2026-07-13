8a20d294b1df787800955cc3f2941fb0aaf579a7
Aisha Amir <aishaamirkhann@gmail.com>
2026-07-14 01:49:44 +0500

    Add files via upload

---
+++ Assigment4/datetime.py
@@ -0,0 +1,12 @@
+import datetime
+import time
+
+print(" Current Date & Time = ", datetime.datetime.now())
+
+print(" Today Date = ", datetime.date.today())
+
+print("Program Started")
+
+time.sleep(3)
+
+print("Program Finished")
