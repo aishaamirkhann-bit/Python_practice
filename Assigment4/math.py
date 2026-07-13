8e300c240acd1a6131f83080701492f22cbe5da2
Aisha Amir <aishaamirkhann@gmail.com>
2026-07-14 01:26:36 +0500

    Add math operations and area calculation in math.py

---
+++ Assigment4/math.py
@@ -0,0 +1,12 @@
+import math
+
+print("sqaure root of 144 =" , math.sqrt(144))
+print(" 7 power 4 = ", math.pow(7,4))
+print(" value of pi = ", math.pi)
+
+
+radius = 10
+area = math.pi * radius * radius
+print("Area of circle = ", area)
+
+
