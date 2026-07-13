c2b6bcc05534ae7b7d45358b6ca26f76fd8c042b
Aisha Amir <aishaamirkhann@gmail.com>
2026-07-14 04:09:44 +0500

    Create college.py

---
+++ Assigment5 data structure/college.py
@@ -0,0 +1,9 @@
+def Welcome():
+    print("Welcome to College")
+    
+def display_students(student_list):
+    student_list = ["Aisha","Gully","Aiman","Areeba", "Adan"]    
+    print("Student List = ", student_list)
+    
+def total_students(student_list):
+    return len(student_list)
