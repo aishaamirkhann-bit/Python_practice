a10f0efd669f0162ef5930ce703604803cb62fcd
Aisha Amir <aishaamirkhann@gmail.com>
2026-07-14 00:13:54 +0500

    Add files via upload

---
+++ temp_conversion.py
@@ -0,0 +1,13 @@
+x=float(input("enter the temperature"))
+celsius = (x-32)*5/9
+fahrenheit = (x*9/5)+32
+print("your temperature in celsius", celsius, "C")
+print("your temperature in fahrenheit", fahrenheit, "F")
+
+if celsius =="c":
+    print("your temperature in fahrenheit", (x*9/5)+32, "F")
+else:
+    print("your temperature in celsius", (x-32)*5/9, "C")    
+    
+    
+
